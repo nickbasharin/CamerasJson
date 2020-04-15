6d121b8f7fd786063ccc74c691271040d144aa88
nickbasharin <bni1284@gmail.com>
2020-04-15 17:33:21 +0300

    adding project

---
+++ Assets/Scripts/CameraReader.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleJSON;
+
+public class CameraReader : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    public GameObject camMesh;
+
+    void Start()
+    {
+        var jsonParse = JSON.Parse(jsonFile.text);
+        float px, py, pz, ox, oy, oz, ow;
+        int camAmount = jsonParse[0]["reconstruction"]["images_number"].AsInt;
+        for (int camNumber = 0; camNumber < camAmount; camNumber++)
+        {
+            px = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["position"][0].AsFloat;
+            py = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["position"][1].AsFloat;
+            pz = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["position"][2].AsFloat;
+            ox = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["orientation"][0].AsFloat;
+            oy = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["orientation"][1].AsFloat;
+            oz = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["orientation"][2].AsFloat;
+            ow = jsonParse[0]["reconstruction"]["images"][camNumber]["image"]["camera_description"]["orientation"][3].AsFloat;
+            GameObject newCam = Instantiate(camMesh, transform);
+            newCam.transform.localPosition = new Vector3(px, py, pz);
+            newCam.transform.localRotation = new Quaternion(ox, oy, oz, ow);
+            newCam.transform.localPosition = newCam.transform.InverseTransformPoint(new Vector3(0,0,0));
+        }
+    }
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour
+{
+
+    public float rotSpeed = 3;
+    public float scaleSpeed = 3;
+    public GameObject scaleObj;
+    float scaleO;
+
+    void OnMouseDrag()
+    {
+        float rotX = Input.GetAxis("Mouse X") * rotSpeed * Mathf.Deg2Rad;
+        float rotY = Input.GetAxis("Mouse Y") * rotSpeed * Mathf.Deg2Rad;
+        transform.RotateAround(Vector3.up, -rotX);
+        transform.RotateAround(Vector3.right, rotY);
+    }
+
+    void OnGUI()
+    {
+        scaleO = scaleObj.transform.localScale.x;
+        scaleO += Input.mouseScrollDelta.y * scaleSpeed * Time.deltaTime * scaleO;
+        if (scaleO < 0.03) scaleO = 0.03f;
+        if (scaleO > 2) scaleO = 2f;
+        scaleObj.transform.localScale = new Vector3(scaleO, scaleO, scaleO);
+    }
+}
+
